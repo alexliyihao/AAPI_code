5f422633ab3d820d80ca8fa65682c2cedc7d4313
HenryNebula <c-huang2015011098@hotmail.com>
2020-10-13 23:31:51 -0400

    add mask/annotation bi-directional transformation functions, comments and examples needed

--- ml_core/utils/annotations.py
+++ ml_core/utils/annotations.py
@@ -1,5 +1,8 @@
 from lxml import etree, objectify
 from shapely.geometry import Polygon
+from shapely.affinity import affine_transform
+import cv2
+import numpy as np
 
 
 class ASAPAnnotation:
@@ -153,3 +156,119 @@ def load_asap_annotations_from_xml(xml_path):
 
     return asap_annotations
 
+
+def annotation_to_mask(annotations, label_info, mask_shape, upper_left_coordinates):
+    """
+    Convert a polygon or multipolygon list back to an image mask ndarray
+
+    Parameters
+    ----------
+    annotations : List[ASAPAnnotation]
+        list of annotations
+    label_name_to_id
+    mask_shape
+    upper_left_coordinates
+
+    Returns
+    -------
+
+    """
+
+    if mask_shape[-1] != 3 and len(mask_shape) == 2:
+        # add the third channel if needed
+        mask_shape = [*mask_shape, 3]
+
+    upper_left_x, upper_left_y = upper_left_coordinates
+    img_mask = np.zeros(mask_shape, np.uint8)
+
+    for label_row in label_info.itertuples():
+        # manipulate every label individually
+        label_name = label_row.label_name
+
+        polygons = [a.geometry for a in annotations if label_name == a.group_name]
+        # see https://shapely.readthedocs.io/en/latest/manual.html#affine-transformations
+        transform_matrix = [1, 0, 0, 1, -upper_left_x, -upper_left_y]
+        polygons = list(map(lambda p: affine_transform(p, transform_matrix), polygons))
+
+        # function to round and convert to int
+        round_coords = lambda x: np.array(x).round().astype(np.int32)
+        exteriors = [round_coords(poly.exterior.coords) for poly in polygons]
+        interiors = [round_coords(pi.coords) for poly in polygons
+                     for pi in poly.interiors]
+        cv2.fillPoly(img_mask, interiors, 0)
+        cv2.fillPoly(img_mask, exteriors, label_row.label)
+
+    return img_mask
+
+
+def mask_to_annotation(mask, label_info, upper_left_coordinates, min_area=10):
+    """
+    Convert a mask ndarray (binarized image) to Multipolygons
+    See Also: https://michhar.github.io/masks_to_polygons_and_back/
+    Parameters
+    ----------
+    mask
+    label_info
+    upper_left_coordinates
+
+    Returns
+    -------
+
+    """
+
+    def mask_to_polygon(binary_mask):
+        image, contours, hierarchy = cv2.findContours(cv2.convertScaleAbs(binary_mask),
+                                                      cv2.RETR_EXTERNAL,
+                                                      cv2.CHAIN_APPROX_TC89_L1)
+
+        if not contours:
+            return None
+
+        # only use the shell to construct polygons,
+        # since the frontend doesn't support annotations with holes
+        all_polygons = []
+        for idx, cnt in enumerate(contours):
+            if cv2.contourArea(cnt) >= min_area:
+                assert cnt.shape[1] == 1
+                poly = Polygon(shell=cnt[:, 0, :])
+                all_polygons.append(poly)
+        return all_polygons
+
+    annotations = []
+    upper_left_x, upper_left_y = upper_left_coordinates
+
+    mask_2d = np.array(mask)[..., 0]
+    for label_row in label_info.itertuples():
+
+        if label_row.label == 0:
+            # skip the background
+            continue
+
+        binary_mask = np.array(mask_2d == label_row.label, dtype=np.uint8)
+        polygons = mask_to_polygon(binary_mask)
+        if polygons is not None:
+            # apply reverse transformation to original coordinates
+            transform_matrix = [1, 0, 0, 1, upper_left_x, upper_left_y]
+            polygons = map(lambda p: affine_transform(p, transform_matrix), polygons)
+
+            label_name = label_row.label_name
+            annotations += [ASAPAnnotation(geometry=p,
+                                           annotation_name=f"{label_name}_{i}",
+                                           group_name=label_name,
+                                           render_color=label_row.color)
+                            for i, p in enumerate(polygons)]
+    return annotations
+
+
+def create_annotation_file(annotations, filename):
+    annotation_xmls = list(map(lambda x: x.to_xml(), annotations))
+    group_xmls = ASAPAnnotation.generate_annotation_groups(annotations)
+
+    root = etree.Element("ASAP_Annotations")
+    annotation_root = etree.SubElement(root, "Annotations")
+    annotation_group_root = etree.SubElement(root, "AnnotationGroups")
+
+    list(map(lambda x: annotation_root.append(x), annotation_xmls))
+    list(map(lambda x: annotation_group_root.append(x), group_xmls))
+
+    write_xml_to_file(filename, root)
