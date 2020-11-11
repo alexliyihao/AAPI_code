97ad2130760d8cd80670f467a8169c5a3b16cfbc
HenryNebula <c-huang2015011098@hotmail.com>
2020-11-10 20:10:14 -0500

    add comments for api

--- ml_core/api.py
+++ ml_core/api.py
@@ -25,7 +25,32 @@ def segment_ROI(ROIs,
                 label_info=None,
                 batch_size=64,
                 return_masks=False):
+    """
+    Produce segmentation results on unseen ROIs and return annotations encoded in ASAP format
+    Parameters
+    ----------
+    ROIs: List[PIL.Image]
+        list of region-of-interest, encoded in PIL Image format
+    upper_left_coords: List[Tuple]
+        list of upper left coordinates for every ROI, i.e., (minx, miny) on level 0.
+    label_info: pd.DataFrame
+        class labels and other attributes, use default_label_info as default
+    batch_size: int
+        batch size for constructing in memory dataloader
+    return_masks: bool
+        if True, will also return a list of masks for ROIs. See below for details.
 
+    Returns
+    -------
+    xml_annotation: str
+        a string encoding merged ASAP annotation of these ROIs
+
+    predicted_masks: List[List[numpy.array]], optional
+        a nested list containing predicted masks for every ROI;
+        every item is a list containing masks for every label class,
+        ordered in the same order as labels in label_info dataframe.
+
+    """
     if label_info is None:
         label_info = default_label_info
     predicted_masks, annotations = predict_on_ROI(ROIs, upper_left_coords, label_info, batch_size)
@@ -38,6 +63,29 @@ def segment_WSI(slide_path,
                 label_info=None,
                 batch_size=64,
                 return_masks=False):
+    """
+    Produce segmentation results on one unseen slide and return annotations encoded in ASAP format
+
+    Parameters
+    ----------
+    slide_path: str
+        path to the unseen tif or svs format slide
+    label_info: pd.DataFrame
+        class labels and other attributes, use default_label_info as default
+    batch_size: int
+        batch size for constructing in memory dataloader
+    return_masks: bool
+        if True, will also return a list of masks for ROIs. See below for details.
+
+    Returns
+    -------
+    xml_annotation: str
+        a string encoding merged ASAP annotation of these ROIs
+    predicted_masks: List[List[numpy.array]], optional
+        a nested list containing predicted masks for this slide;
+        every item is a list containing masks for every label class,
+        ordered in the same order as labels in label_info dataframe.
+    """
 
     if label_info is None:
         label_info = default_label_info
